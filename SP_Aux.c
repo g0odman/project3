6a592db9e9f5bcdde0707c2b74aa388dd05d47bb
Alexander Ar-Staelin <alexander@Alexanders-MacBook-Pro.local>
2015-12-16 20:59:15 +0200

    played around with comments

--- SP_Aux.c
+++ SP_Aux.c
@@ -8,23 +8,26 @@
  *  all necsesary things.
  */
 void parse(char * line){
+	//check whether exit command:
     if(strncmp(line,"(<>)", 4) == 0 && strlen(line) == 5){
         if(printf("Exiting...\n")!= 0){
             exit(EXIT_FAILURE);
         }
         exit(EXIT_SUCCESS);
     }
-    bool * valid = malloc(sizeof(bool));
-    *valid = true;
-    //make trees and validate that they were succesfull:
+
+    //make tree and validate that they were succesfull:
+    bool valid = true;
     SP_TREE *root = split(line);
-    double out =  spTreeEval(root,valid);
-    if(*valid)
+
+    //evaluate:
+    double out =  spTreeEval(root,&valid);
+    if(valid)
         printf("Res = %f\n", out);
     else
         printf("Invalid Result\n");
+
     spTreeDestroy(root);
-    free(valid);
 }
 
 SP_TREE *split(char *line){
@@ -38,14 +41,13 @@ SP_TREE *split(char *line){
 
     //j is the number of brackets seen, i is the current place
     int j = 1,i=1;
-
-    while(j > 0){
-        //recursively parse children:
+    while(j > 0){  //recursively parse children:
         if(line[i] == '(' && (++j) == 2){
+        	//if closed brackets on child, parse it:
             if(!spTreePush(new,split(line+i))){
                 if(printf("Unexpected error occured!")!=0)
                     exit(EXIT_FAILURE);
-                exit(EXIT_SUCCESS); //why is this a success?
+                exit(EXIT_SUCCESS);
             }
         }
         //Go up one level
@@ -54,7 +56,7 @@ SP_TREE *split(char *line){
         i++;
     }
 
-    //Copy the relevant string only:
+    //Copy the relevant string for this node:
     int length = 1;
     while(line[length] != '(' && line[length] != ')') { length++; }
     length--;
@@ -63,22 +65,23 @@ SP_TREE *split(char *line){
     temp[length] = '\0';
     if(!setValue(new,temp))
         exit(EXIT_SUCCESS); //why success?
-    free(temp);  //need to free!!
+    free(temp);
 
     return new;
 }
 
-double operate(double x, double y, SP_TREE_TYPE op,bool * valid){
+double operate(double x, double y, SP_TREE_TYPE op, bool * valid){
     *valid = (isValid(op,x,y) ? *valid : false);
+    //perform relevant op on inputs
     switch (op){
         case PLUS:
-            return x+y;
+            return x + y;
         case MINUS:
             return  x - y;
         case MULTIPLICATION:
             return  x * y;
         case DIVISION:
-            return  x/y;
+            return  x / y;
         case DOLLAR:
             return ((y-x+1)*(y+x))/2;
         default :
@@ -87,6 +90,7 @@ double operate(double x, double y, SP_TREE_TYPE op,bool * valid){
 }
 
 bool isValid(SP_TREE_TYPE op, double x, double y){
+	//test relevant case for op:
     switch(op){
         case PLUS:
         case MINUS:
@@ -103,18 +107,20 @@ bool isValid(SP_TREE_TYPE op, double x, double y){
 
 double spTreeEval(SP_TREE *tree, bool * valid){
 
-    //leaf:
+    //leaf, return value:
     if(tree->type == NUMBER){
         return atoi(getRootStr(tree));
     }
 
-    //calculate op on children:
+    //otherwise, calculate op on first child, first:
     double out = spTreeEval(tree->children[0],valid);
 
     //In case of negative number
     if(tree->size ==1)
         return operate(0,out,tree->type,valid);
 
+    //then continue recursively calculating children,
+    //and performing the op on them
     for(int i=1; i < tree->size; i++){
         double temp = spTreeEval(tree->children[i],valid);
         out = operate(out,temp,tree->type,valid);
